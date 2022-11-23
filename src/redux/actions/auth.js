250120d31a0c6a98c7fb5baeb8eac57853cd3c99
DvidGs <dgallegos9207@gmail.com>
2022-11-23 15:36:11 -0500

    UserAuthentication(ProcessForm, ActiveAccount)

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -1,16 +1,24 @@
 import {
     SIGNUP_SUCCESS,
     SIGNUP_FAIL,
+    ACTIVATION_SUCCESS,
+    ACTIVATION_FAIL,
+    SET_AUTH_LOADING,
+    REMOVE_AUTH_LOADING,
 } from './types'
 
 import axios from 'axios'
 
 export const signup = (first_name, last_name, email, password, re_password) => async dispatch => {
+    dispatch({
+        type: SET_AUTH_LOADING
+    });
+
     const config = {
         headers: {
             'Content-Type' : 'application/json'
         }
-    }
+    };
 
     const body = JSON.stringify({
         first_name,
@@ -23,20 +31,68 @@ export const signup = (first_name, last_name, email, password, re_password) => a
     try {
         const res = await axios.post(`${process.env.REACT_APP_API_URL}/auth/users/`, body, config)
         
-        if (res.status === 201){
+        if (res.status === 200){
             dispatch({
                 type: SIGNUP_SUCCESS,
                 payload: res.data
-            })
+            });
         } else {
             dispatch({
                 type: SIGNUP_FAIL,
-            })
+            });
         }
+        dispatch({
+            type: REMOVE_AUTH_LOADING
+        });
     }
-    catch (error) {
+    catch (err) {
         dispatch({
             type: SIGNUP_FAIL,
-        })
+        });
+        dispatch({
+            type: REMOVE_AUTH_LOADING
+        });
+    }
+}
+
+export const activate = (uid, token) => async dispatch => {
+    dispatch({
+        type: SET_AUTH_LOADING
+    });
+    
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    const body = JSON.stringify({
+        uid,
+        token
+    });
+
+    try {
+        const res = await axios.post(`${process.env.REACT_APP_API_URL}/auth/users/activation/`, body, config);
+        
+        if (res.status === 201) {
+            dispatch({
+                type: ACTIVATION_SUCCESS
+            });
+        } else {
+            dispatch({
+                type: ACTIVATION_FAIL
+            });
+        }
+        dispatch({
+            type: REMOVE_AUTH_LOADING
+        });
+    }
+    catch(err) {
+        dispatch({
+            type: ACTIVATION_FAIL
+        });
+        dispatch({
+            type: REMOVE_AUTH_LOADING
+        });
     }
 }
